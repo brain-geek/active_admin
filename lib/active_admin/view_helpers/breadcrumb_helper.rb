7244efa855c40f436aadaa5af19ffb0a3e73c180
Alex Rozumey <brain-geek@yandex.ua>
2012-05-24 18:01:04 +0300

    Add singularization to prev patch

--- lib/active_admin/view_helpers/breadcrumb_helper.rb
+++ lib/active_admin/view_helpers/breadcrumb_helper.rb
@@ -12,7 +12,7 @@ module ActiveAdmin
 					name = ""
 					if part =~ /^\d/ && parent = parts[index - 1]
 						begin
-							parent_class = active_admin_namespace.resources.find_by_key(parent.titleize).resource
+							parent_class = active_admin_namespace.resources.find_by_key(parent.singularize.titleize).resource
 							obj = parent_class.find(part.to_i)
 							name = obj.display_name if obj.respond_to?(:display_name)
 						rescue
