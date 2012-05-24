028e1064f159ed323eac0731ce44f788a8aac52b
Alex Rozumey <brain-geek@yandex.ua>
2012-05-24 17:19:53 +0300

    Do not make number in breadcrumb link if controller does not respond to show action.

--- lib/active_admin/view_helpers/breadcrumb_helper.rb
+++ lib/active_admin/view_helpers/breadcrumb_helper.rb
@@ -19,10 +19,22 @@ module ActiveAdmin
 						end
 					end
 					name = part.titlecase if name == ""
-					begin
-						crumbs << link_to( I18n.translate!("activerecord.models.#{part.singularize}", :count => 2), "/" + parts[0..index].join('/'))
-					rescue I18n::MissingTranslationData
-						crumbs << link_to( name, "/" + parts[0..index].join('/'))
+
+					#if resource is defined, this is "show" link and it does not respond to "show"
+					if part =~ /^\d/ && parent = parts[index - 1] && 
+							active_admin_namespace.resources.find_by_key(parent.singularize.titleize) && 
+							!active_admin_namespace.resources.find_by_key(parent.singularize.titleize).respond_to?(:show)
+						begin
+							crumbs << I18n.translate!("activerecord.models.#{part.singularize}")
+						rescue I18n::MissingTranslationData
+							crumbs << name
+						end
+					else
+						begin
+							crumbs << link_to( I18n.translate!("activerecord.models.#{part.singularize}", :count => 2), "/" + parts[0..index].join('/'))
+						rescue I18n::MissingTranslationData
+							crumbs << link_to( name, "/" + parts[0..index].join('/'))
+						end
 					end
 				end
 				crumbs
